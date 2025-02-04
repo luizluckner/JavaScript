0ff96fac6dabf31111516e39f1d25f0a8e97f149
Luiz Eduardo Luckner Costa <40869544+luizluckner@users.noreply.github.com>
2025-02-04 19:29:27 -0300

    Contiuação dos estudos de JS

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60
+// console.log(`A velocidade do carro é ${vel}`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Sua velocidade é ${vel}`)
+} else {
+    console.log(`Muito bem!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
